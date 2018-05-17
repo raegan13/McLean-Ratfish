a4539d6b91d2198f32727016f0eb96b111b1900d
raegan13 <38798434+raegan13@users.noreply.github.com>
2018-05-17 00:16:44 -0700

    Update Ratfish_data_scripts.R

--- scripts/Ratfish_data_scripts.R
+++ scripts/Ratfish_data_scripts.R
@@ -1,3 +1,10 @@
 ratfish_data <- read.csv("data/Copy of JonMcLean_Ratfish_Tow_Location_Densities(4824).csv")
 
 library(tidyverse)
+
+# I wonder if this will work for graphing...
+
+ggplot(ratfish_data) +
+geom_point(aes(x = StartLatitude_DD, y = Ratfish_BiomassDensity))
+
+# Maybe 1-3 other graphs depending on what latitude/longitude values matter
